7d1579d8ae2ce92f46417214431832a511b3413c
mhoughton26 <mhoughton26@hb.edu>
2026-01-25 15:03:16 -0500

    create constant classes for each robot and configs for them, no values yet

--- src/main/java/frc/robot/subsystems/drive/DriveSubsystemConfigurations.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSubsystemConfigurations.java
@@ -1,31 +1,74 @@
 package frc.robot.subsystems.drive;
 
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecondPerSecond;
+
 import edu.wpi.first.units.measure.AngularAcceleration;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.LinearAcceleration;
 import edu.wpi.first.units.measure.LinearVelocity;
 
-public class DriveSubsystemConfigurations {
-    
+public final class DriveSubsystemConfigurations {
 
+    // constants for different robots, separated into classes for organization
+    // not named following [ROBOTNAME]_KS template because every call will be [ROBOTNAME]Constants.KS
+    // since the constant will never be called inside its own class
+    // thus making putting the robot name before every constant redundant
+    private static final class MozartConstants {
+        private static final double KS = 0;
+        private static final double KV = 0;
+        private static final double DRIVE_P = 0;
+        private static final double DRIVE_D = 0;
+        private static final double TURN_P = 0;
+        private static final double TURN_D = 0;
+        private static final double HEADING_P = 0;
+        private static final double HEADING_D = 0;
+        private static final Distance TRACK_WIDTH = Inches.of(0);
+        private static final Distance TRACK_LENGTH = Inches.of(0);
+        private static final LinearAcceleration MAX_ACCELERATION = MetersPerSecondPerSecond.of(0);
+        private static final LinearVelocity MAX_SPEED = MetersPerSecond.of(0);
+        private static final AngularAcceleration MAX_ANGULAR_ACCELERATION = RadiansPerSecondPerSecond.of(0);
+    }
+
+    private static final class BubblesConstants {
+        private static final double KS = 0;
+        private static final double KV = 0;
+        private static final double DRIVE_P = 0;
+        private static final double DRIVE_D = 0;
+        private static final double TURN_P = 0;
+        private static final double TURN_D = 0;
+        private static final double HEADING_P = 0;
+        private static final double HEADING_D = 0;
+        private static final Distance TRACK_WIDTH = Inches.of(0);
+        private static final Distance TRACK_LENGTH = Inches.of(0);
+        private static final LinearAcceleration MAX_ACCELERATION = MetersPerSecondPerSecond.of(0);
+        private static final LinearVelocity MAX_SPEED = MetersPerSecond.of(0);
+        private static final AngularAcceleration MAX_ANGULAR_ACCELERATION = RadiansPerSecondPerSecond.of(0);
+    }
+
+    // architecture for creating a DriveConfig
     /*
-     * A record is a shorthand way of making a class that's only really meant to store data.
-     * The Java compiler will auto-generate a constructor and instance variables that match the parameters.
+     * A record is a shorthand way of making a class that's only really meant to
+     * store data.
+     * The Java compiler will auto-generate a constructor and instance variables
+     * that match the parameters.
      * As a result, the code is shorter, cleaner, and easier to read!
      */
-    public record DriveConfig (
-        double kS,
-        double kV,
-        double driveP,
-        double driveD,
-        double turnP,
-        double turnD,
-        double headingP,
-        double headingD,
-        Distance trackWidth,
-        Distance trackLength,
-        LinearAcceleration maxAccel,
-        LinearVelocity maxlLinearSpeed,
-        AngularAcceleration maxAngularAccel   
-    ) {}
+    public record DriveConfig(
+            double kS,
+            double kV,
+            double driveP,
+            double driveD,
+            double turnP,
+            double turnD,
+            double headingP,
+            double headingD,
+            Distance trackWidth,
+            Distance trackLength,
+            LinearAcceleration maxAccel,
+            LinearVelocity maxlLinearSpeed,
+            AngularAcceleration maxAngularAccel) {
+    }
 }
