6e087018a051d9a1f31f08206b792c53381a00ca
mhoughton26 <mhoughton26@hb.edu>
2026-01-25 15:06:57 -0500

    ACTUALLY create constant classes for robot and configs for them, no values yet

--- src/main/java/frc/robot/subsystems/drive/DriveSubsystemConfigurations.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSubsystemConfigurations.java
@@ -13,7 +13,8 @@ import edu.wpi.first.units.measure.LinearVelocity;
 public final class DriveSubsystemConfigurations {
 
     // constants for different robots, separated into classes for organization
-    // not named following [ROBOTNAME]_KS template because every call will be [ROBOTNAME]Constants.KS
+    // not named following [ROBOTNAME]_KS template because every call will be
+    // [ROBOTNAME]Constants.KS
     // since the constant will never be called inside its own class
     // thus making putting the robot name before every constant redundant
     private static final class MozartConstants {
@@ -55,6 +56,10 @@ public final class DriveSubsystemConfigurations {
      * The Java compiler will auto-generate a constructor and instance variables
      * that match the parameters.
      * As a result, the code is shorter, cleaner, and easier to read!
+     * 
+     * Any field in the class can be called via
+     * [driveconfigobjectname].[fieldname]();
+     * (e.g. mozartDriveConfig.kS();)
      */
     public record DriveConfig(
             double kS,
@@ -71,4 +76,16 @@ public final class DriveSubsystemConfigurations {
             LinearVelocity maxlLinearSpeed,
             AngularAcceleration maxAngularAccel) {
     }
+
+    public static final DriveConfig mozartDriveConfig = new DriveConfig(MozartConstants.KS, MozartConstants.KV,
+            MozartConstants.DRIVE_P, MozartConstants.DRIVE_D, MozartConstants.TURN_P, MozartConstants.TURN_D,
+            MozartConstants.HEADING_P, MozartConstants.HEADING_D, MozartConstants.TRACK_WIDTH,
+            MozartConstants.TRACK_LENGTH, MozartConstants.MAX_ACCELERATION, MozartConstants.MAX_SPEED,
+            MozartConstants.MAX_ANGULAR_ACCELERATION);
+
+    public static final DriveConfig bubblesDriveConfig = new DriveConfig(BubblesConstants.KS, BubblesConstants.KV,
+            BubblesConstants.DRIVE_P, BubblesConstants.DRIVE_D, BubblesConstants.TURN_P, BubblesConstants.TURN_D,
+            BubblesConstants.HEADING_P, BubblesConstants.HEADING_D, BubblesConstants.TRACK_WIDTH,
+            BubblesConstants.TRACK_LENGTH, BubblesConstants.MAX_ACCELERATION, BubblesConstants.MAX_SPEED,
+            BubblesConstants.MAX_ANGULAR_ACCELERATION);
 }
