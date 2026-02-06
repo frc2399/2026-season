3422cb12ef16285b2814bf8068de5f60672a008f
Baheya Qoran <bqoran29@hb.edu>
2026-02-05 20:59:49 -0500

    in progress

--- src/main/java/frc/robot/constants/FieldConstants.java
+++ src/main/java/frc/robot/constants/FieldConstants.java
@@ -1,3 +1,13 @@
 package frc.robot.constants;
 
-public final class FieldConstants {}
+import edu.wpi.first.units.measure.Distance;
+import static edu.wpi.first.units.Units.*;
+
+public final class FieldConstants {
+    public static class AllianceZoneBoundaries {
+    public static final Distance RED_ZONE_INNER_BOUNDARY_X = Inches.of(158.61);
+    public static final Distance RED_ZONE_OUTER_BOUNDARY_X = Inches.of(0);
+    public static final Distance BLUE_ZONE_INNER_BOUNDARY_X = Inches.of(0);
+    }
+}
+
