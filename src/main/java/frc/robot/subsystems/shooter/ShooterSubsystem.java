c893d9190b8646d5f48007c568c5cd2445ccf226
Olivia Kude <okude28@hb.edu>
2026-02-05 19:00:10 -0500

    I created the first pass of the shooter subsystem

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase {
+    private ShooterIO io;
+
+    public ShooterSubsystem(ShooterIO io) {
+        this.io = io; 
+    }
+
+    public Command shoot() {
+        return this.run(() -> io.runShooter()).withName("runShooter");
+    }
+}
