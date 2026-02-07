665d9888c1e9d83f308967cb9180edc1e3e42763
Vivian Qi <vqi26@hb.edu>
2026-02-07 16:36:47 -0500

    added intake arm folder with implementation

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2026-season";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 88;
-    public static final String GIT_SHA = "e569600eb35e142768dc99ec3a5d4220e097ac8e";
-    public static final String GIT_DATE = "2026-02-05 18:46:58 EST";
-    public static final String GIT_BRANCH = "intake-hardware";
-    public static final String BUILD_DATE = "2026-02-05 18:48:39 EST";
-    public static final long BUILD_UNIX_TIME = 1770335319595L;
+    public static final int GIT_REVISION = 155;
+    public static final String GIT_SHA = "69f270298093342fcba15510b283ffaf16e8cad6";
+    public static final String GIT_DATE = "2026-02-07 15:38:47 EST";
+    public static final String GIT_BRANCH = "intake-arm-implementation";
+    public static final String BUILD_DATE = "2026-02-07 16:36:20 EST";
+    public static final long BUILD_UNIX_TIME = 1770500180585L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
