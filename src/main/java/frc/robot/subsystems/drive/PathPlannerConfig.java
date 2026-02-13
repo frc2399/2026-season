555553b46adfb8962ee1de6ad1fb97c8746d8c91
Aashvi Jagetia <ajagetia26@hb.edu>
2026-02-12 20:04:31 -0500

    cnflict

--- src/main/java/frc/robot/subsystems/drive/PathPlannerConfig.java
+++ src/main/java/frc/robot/subsystems/drive/PathPlannerConfig.java
@@ -29,79 +29,79 @@ public final class PathPlannerConfig {
     public static final Translation2d MODULE_OFFSETS;
 
 
-    static {
-        switch (SubsystemFactory.getRobotType()) {
-            case BUBBLES:
-                ROBOT_MASS = 
-                ROBOT_MOI = KilogramSquareMeters.of(6.883);
-                WHEEL_RADIUS = 
-                MAX_SPEED = 4.3;
-                WHEEL_COF = 
-                DRIVE_MOTOR = 
-                DRIVE_CURRENT_LIMIT = 
-                NUM_MOTORS = 
-                MODULE_OFFSETS = 
-                break;
-            case MOZART:
-                ROBOT_MASS = 
-                ROBOT_MOI = 
-                WHEEL_RADIUS = 
-                MAX_SPEED = 0;
-                WHEEL_COF = 
-                DRIVE_MOTOR = 
-                DRIVE_CURRENT_LIMIT = 
-                NUM_MOTORS = 
-                MODULE_OFFSETS = 
-                break;
-            case BETA:
-                ROBOT_MASS = 
-                ROBOT_MOI = 
-                WHEEL_RADIUS = 
-                MAX_SPEED = 0;
-                WHEEL_COF = 
-                DRIVE_MOTOR = 
-                DRIVE_CURRENT_LIMIT = 
-                NUM_MOTORS = 
-                MODULE_OFFSETS = 
-                break;
-            case COMP:
-                ROBOT_MASS = Kilograms.of(0);
-                ROBOT_MOI = KilogramSquareMeters.of(0);
-                WHEEL_RADIUS = 0;
-                MAX_SPEED = 0;
-                WHEEL_COF = 0;
-                DRIVE_MOTOR = 0;
-                DRIVE_CURRENT_LIMIT = 0;
-                NUM_MOTORS = 0;
-                MODULE_OFFSETS = 0;
-                break;
-            case SIM:
-                kS = 0;
-                kV = 0;
-                DRIVE_P = 0;
-                DRIVE_D = 0;
-                TURN_P = 0;
-                TURN_D = 0;
-                HEADING_P = 0;
-                HEADING_D = 0;
-                PINION_TEETH = 0;
-                TRACK_WIDTH = Inches.of(0);
-                TRACK_LENGTH = Inches.of(0);
-                MAX_ACCELERATION = MetersPerSecondPerSecond.of(0);
-                MAX_SPEED = MetersPerSecond.of(0);
-                MAX_ANGULAR_VELOCITY = RadiansPerSecond.of(0);
-                break;
-            default:
-                ROBOT_MASS = Kilograms.of(0);
-                ROBOT_MOI = KilogramSquareMeters.of(0);
-                WHEEL_RADIUS = 0;
-                MAX_SPEED = 0;
-                WHEEL_COF = 0;
-                DRIVE_MOTOR = 0; 
-                DRIVE_CURRENT_LIMIT = 0;
-                NUM_MOTORS = 0;
-                MODULE_OFFSETS = 0;
-                break;
-        }
-    }
-}
+//     static {
+//         switch (SubsystemFactory.getRobotType()) {
+//             case BUBBLES:
+//                 ROBOT_MASS = 
+//                 ROBOT_MOI = KilogramSquareMeters.of(6.883);
+//                 WHEEL_RADIUS = 
+//                 MAX_SPEED = 4.3;
+//                 WHEEL_COF = 
+//                 DRIVE_MOTOR = 
+//                 DRIVE_CURRENT_LIMIT = 
+//                 NUM_MOTORS = 
+//                 MODULE_OFFSETS = 
+//                 break;
+//             case MOZART:
+//                 ROBOT_MASS = 
+//                 ROBOT_MOI = 
+//                 WHEEL_RADIUS = 
+//                 MAX_SPEED = 0;
+//                 WHEEL_COF = 
+//                 DRIVE_MOTOR = 
+//                 DRIVE_CURRENT_LIMIT = 
+//                 NUM_MOTORS = 
+//                 MODULE_OFFSETS = 
+//                 break;
+//             case BETA:
+//                 ROBOT_MASS = 
+//                 ROBOT_MOI = 
+//                 WHEEL_RADIUS = 
+//                 MAX_SPEED = 0;
+//                 WHEEL_COF = 
+//                 DRIVE_MOTOR = 
+//                 DRIVE_CURRENT_LIMIT = 
+//                 NUM_MOTORS = 
+//                 MODULE_OFFSETS = 
+//                 break;
+//             case COMP:
+//                 ROBOT_MASS = Kilograms.of(0);
+//                 ROBOT_MOI = KilogramSquareMeters.of(0);
+//                 WHEEL_RADIUS = 0;
+//                 MAX_SPEED = 0;
+//                 WHEEL_COF = 0;
+//                 DRIVE_MOTOR = 0;
+//                 DRIVE_CURRENT_LIMIT = 0;
+//                 NUM_MOTORS = 0;
+//                 MODULE_OFFSETS = 0;
+//                 break;
+//             case SIM:
+//                 kS = 0;
+//                 kV = 0;
+//                 DRIVE_P = 0;
+//                 DRIVE_D = 0;
+//                 TURN_P = 0;
+//                 TURN_D = 0;
+//                 HEADING_P = 0;
+//                 HEADING_D = 0;
+//                 PINION_TEETH = 0;
+//                 TRACK_WIDTH = Inches.of(0);
+//                 TRACK_LENGTH = Inches.of(0);
+//                 MAX_ACCELERATION = MetersPerSecondPerSecond.of(0);
+//                 MAX_SPEED = MetersPerSecond.of(0);
+//                 MAX_ANGULAR_VELOCITY = RadiansPerSecond.of(0);
+//                 break;
+//             default:
+//                 ROBOT_MASS = Kilograms.of(0);
+//                 ROBOT_MOI = KilogramSquareMeters.of(0);
+//                 WHEEL_RADIUS = 0;
+//                 MAX_SPEED = 0;
+//                 WHEEL_COF = 0;
+//                 DRIVE_MOTOR = 0; 
+//                 DRIVE_CURRENT_LIMIT = 0;
+//                 NUM_MOTORS = 0;
+//                 MODULE_OFFSETS = 0;
+//                 break;
+//         }
+//     }
+// }
