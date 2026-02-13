976780057a43161e015065834b917beba341f363
Baheya Qoran <bqoran29@hb.edu>
2026-02-12 19:32:12 -0500

    switched numbers into variables

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2026-season";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 191;
-    public static final String GIT_SHA = "9beb4bf9a38ae9b3f2460b9c7d8ef5445d7a6191";
-    public static final String GIT_DATE = "2026-02-12 18:32:42 EST";
+    public static final int GIT_REVISION = 192;
+    public static final String GIT_SHA = "9d6be24cb3fd08c839206133c21b0292bd547b2a";
+    public static final String GIT_DATE = "2026-02-12 19:25:29 EST";
     public static final String GIT_BRANCH = "target-hub-or-pass-fuel";
-    public static final String BUILD_DATE = "2026-02-12 19:24:29 EST";
-    public static final long BUILD_UNIX_TIME = 1770942269671L;
-    public static final int DIRTY = 1;
+    public static final String BUILD_DATE = "2026-02-12 19:31:15 EST";
+    public static final long BUILD_UNIX_TIME = 1770942675062L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }
