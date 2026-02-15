18558cb2a3d80be26c46137bfc1e8aabb7495fa0
mhoughton26 <mhoughton26@hb.edu>
2026-02-15 15:51:42 -0500

    add beta shooter hardware + subsystem factory support

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -30,6 +30,9 @@ public final class RobotConstants {
         public static final int GYRO_CAN_ID = 3;
         public static final int SHOOTER_BOTTOM_CAN_ID = 3;
         public static final int SHOOTER_TOP_CAN_ID = 21;
+
+        public static final int SHOOTER_BOTTOM_BETA_CAN_ID = 3;
+        public static final int SHOOTER_TOP_BETA_CAN_ID = 21;
     }
 
     public static class SensorIdConstants {}
