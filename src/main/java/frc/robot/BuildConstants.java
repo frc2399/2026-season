874ec95d34f114c86bbae55384f1cb5f7a2e0695
Aashvi Jagetia <ajagetia26@hb.edu>
2026-02-19 18:43:49 -0500

    merge conflicts

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "2026-season";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 331;
+    public static final String GIT_SHA = "4e9d4312e3f30d52fe3d9d67a8adc8f9183ec20f";
+    public static final String GIT_DATE = "2026-02-15 17:16:49 EST";
+    public static final String GIT_BRANCH = "depot-to-neutral-zone";
+    public static final String BUILD_DATE = "2026-02-15 19:33:01 EST";
+    public static final long BUILD_UNIX_TIME = 1771201981694L;
+    public static final int DIRTY = 1;
+
+    private BuildConstants() {}
+}
