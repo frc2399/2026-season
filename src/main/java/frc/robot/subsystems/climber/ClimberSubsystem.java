07cdf34d519222dd0455bab1f862ea02034aeafd
Vidhi Vakil <vakilv28@hb.edu>
2026-02-21 13:23:38 -0500

    added climberIO methods and removing existing methods

--- src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.climber;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimberSubsystem extends SubsystemBase {
@@ -8,13 +7,5 @@ public class ClimberSubsystem extends SubsystemBase {
 
     public ClimberSubsystem(ClimberIO io) {
         this.io = io;
-    }
-
-    public Command runClimber() {
-        return this.run(() -> io.runClimber()).withName("runClimber");
-    }
-
-    public Command runClimberDefaultBehavior() {
-        return this.run(() -> io.runClimberDefaultBehavior()).withName("runClimberDefaultBeavhior");
     }
 }
