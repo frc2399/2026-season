f847ed573c24ce8778a7a61e50ba1f7b4b49cb8b
Vidhi Vakil <vakilv28@hb.edu>
2026-02-21 16:32:14 -0500

    added methods to climber subsystem from IO

--- src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
@@ -5,6 +5,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ClimberSubsystem extends SubsystemBase {
     private ClimberIO io;
 
+    public void extend() {}
+
+    public void retract() {}
+
+    public void runForwards() {}
+
+    public void runBackwards() {}
+
     public ClimberSubsystem(ClimberIO io) {
         this.io = io;
     }
