3cfcc819f06a2845420964ecc6f3baa37e254713
mhoughton26 <mhoughton26@hb.edu>
2026-03-01 15:34:44 -0500

    some tuning (not done yet)

--- src/main/java/frc/robot/subsystems/intake/IntakeArmHardwareBeta.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeArmHardwareBeta.java
@@ -2,7 +2,9 @@ package frc.robot.subsystems.intake;
 
 import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Degrees;
-import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
 
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.PersistMode;
@@ -15,9 +17,11 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.ClosedLoopConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkFlexConfig;
+import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import frc.robot.constants.RobotConstants;
+import frc.robot.util.TunableNumber;
 
 public class IntakeArmHardwareBeta implements IntakeArmIO {
     // 45:1 ratio
@@ -33,18 +37,17 @@ public class IntakeArmHardwareBeta implements IntakeArmIO {
     private final ClosedLoopConfig intakeArmClosedLoopConfig = new ClosedLoopConfig();
 
     // inversions
-    private static final boolean INTAKE_ARM_MOTOR_INVERTED = false;
+    private static final boolean INTAKE_ARM_MOTOR_INVERTED = true;
     private static final boolean INTAKE_ARM_ENCODER_INVERTED = false;
 
     // conversion factors (45:1 is our gear ratio)
     private static final Angle INTAKE_ARM_POSITION_CONVERSION_FACTOR =
-            Degrees.of(360.0); // this subsystem is in
-    // degrees because it's a
-    // rotational wrist,
-    // and this makes it easier to tune :)
+            Radians.of(2 * Math.PI); // this subsystem is
+    // in
+    // radians
     private static final AngularVelocity INTAKE_ARM_VELOCITY_CONVERSION_FACTOR =
-            DegreesPerSecond.of(
-                    360.0 / 60); // math more explicit to make conversion more understandable
+            RadiansPerSecond.of(
+                    2 * Math.PI / 60); // math more explicit to make conversion more understandable
 
     // pid
     private static final double INTAKE_ARM_P = 0.00;
@@ -54,17 +57,24 @@ public class IntakeArmHardwareBeta implements IntakeArmIO {
     // discord in
     // #programming on 2/26)
     private static final double INTAKE_ARM_KS = 0;
-    private static final double INTAKE_ARM_KV = 0.026;
+    private static final double INTAKE_ARM_KV = 3.20;
     private static final double INTAKE_ARM_KA = 0.00011;
-    private static final double INTAKE_ARM_KCOS = 0.23;
+    private static final double INTAKE_ARM_KCOS = .14;
+
+    private static final ArmFeedforward intakeArmFeedforward =
+            new ArmFeedforward(INTAKE_ARM_KS, INTAKE_ARM_KCOS, INTAKE_ARM_KV, INTAKE_ARM_KA);
+
+    private static final TunableNumber TUNABLE_INTAKE_KCOS =
+            new TunableNumber("intake/arm", INTAKE_ARM_KCOS, true);
+
     /*
      * our arm is rotational, so the impact of gravity changes as we rotate, and our
      * feedforward needs to compensate. the kcos is the factor to compensate by
-     * revlib demands that it can get from what we're logging in (degree/s) to
+     * revlib demands that it can get from what we're logging in (rad/s) to
      * rotations of mechanism / second to accurately compensate. that's kcosratio
      */
     private static final double INTAKE_ARM_KCOS_RATIO =
-            1 / 360; // convert from mechanism degrees to rotations
+            1 / (2 * Math.PI); // convert from mechanism radians to rotations
 
     // output
     private static final double INTAKE_ARM_MIN_OUTPUT = -1;
@@ -94,9 +104,9 @@ public class IntakeArmHardwareBeta implements IntakeArmIO {
         intakeArmSparkFlexConfig
                 .absoluteEncoder
                 .inverted(INTAKE_ARM_ENCODER_INVERTED)
-                .positionConversionFactor(INTAKE_ARM_POSITION_CONVERSION_FACTOR.in(Degrees))
+                .positionConversionFactor(INTAKE_ARM_POSITION_CONVERSION_FACTOR.in(Radians))
                 .velocityConversionFactor(
-                        INTAKE_ARM_VELOCITY_CONVERSION_FACTOR.in(DegreesPerSecond))
+                        INTAKE_ARM_VELOCITY_CONVERSION_FACTOR.in(RadiansPerSecond))
                 .zeroCentered(true);
 
         intakeArmSparkFlexConfig
@@ -105,7 +115,7 @@ public class IntakeArmHardwareBeta implements IntakeArmIO {
                 .pid(INTAKE_ARM_P, INTAKE_ARM_I, INTAKE_ARM_D)
                 .outputRange(INTAKE_ARM_MIN_OUTPUT, INTAKE_ARM_MAX_OUTPUT)
                 .positionWrappingEnabled(true)
-                .positionWrappingInputRange(-180, 180);
+                .positionWrappingInputRange(-Math.PI, Math.PI);
 
         // soft limits are code-enforced limits on where the mechanism can go
         // they're called SOFT limits because the mechanism can still technically go
@@ -113,19 +123,19 @@ public class IntakeArmHardwareBeta implements IntakeArmIO {
         // if the mechanism can't physically go past it, that's a HARD limit/stop
         intakeArmSparkFlexConfig
                 .softLimit
-                .forwardSoftLimit(INTAKE_ARM_FORWARD_MAX_ANGLE.in(Degrees))
+                .forwardSoftLimit(INTAKE_ARM_FORWARD_MAX_ANGLE.in(Radians))
                 .forwardSoftLimitEnabled(INTAKE_ARM_FORWARD_SOFTLIMIT_ENABLED)
-                .reverseSoftLimit(INTAKE_ARM_REVERSE_MAX_ANGLE.in(Degrees))
+                .reverseSoftLimit(INTAKE_ARM_REVERSE_MAX_ANGLE.in(Radians))
                 .reverseSoftLimitEnabled(INTAKE_ARM_REVERSE_SOFTLIMIT_ENABLED);
 
-        intakeArmClosedLoopConfig.feedForward.svacr(
-                INTAKE_ARM_KS,
-                INTAKE_ARM_KV,
-                INTAKE_ARM_KA,
-                INTAKE_ARM_KCOS,
-                INTAKE_ARM_KCOS_RATIO);
+        // intakeArmClosedLoopConfig.feedForward.sva(
+        // INTAKE_ARM_KS,
+        // INTAKE_ARM_KV,
+        // INTAKE_ARM_KA,
+        // INTAKE_ARM_KCOS,
+        // INTAKE_ARM_KCOS_RATIO);
 
-        intakeArmSparkFlexConfig.apply(intakeArmClosedLoopConfig);
+        // intakeArmSparkFlexConfig.apply(intakeArmClosedLoopConfig);
 
         intakeArmSparkFlex =
                 new SparkFlex(
@@ -164,30 +174,48 @@ public class IntakeArmHardwareBeta implements IntakeArmIO {
     @Override
     public void runVelocity(IntakeArmSetpoint setpoint) {
         if (setpoint == IntakeArmSetpoint.DEPLOYED) {
-            System.out.println("hi");
-            intakeArmClosedLoop.setSetpoint(-6, ControlType.kVelocity);
+            // intakeArmClosedLoop.setSetpoint(-6, ControlType.kVelocity);
+            AngularVelocity desiredSpeed =
+                    RadiansPerSecond.of(
+                            -0.1
+                                    + intakeArmFeedforward.calculate(
+                                            intakeArmAbsoluteEncoder.getPosition(), -0.1));
+            intakeArmSparkFlex.set(desiredSpeed.in(RotationsPerSecond));
         } else if (setpoint == IntakeArmSetpoint.STOWED) {
-            intakeArmClosedLoop.setSetpoint(6, ControlType.kVelocity);
+            // intakeArmClosedLoop.setSetpoint(6, ControlType.kVelocity);
+            AngularVelocity desiredSpeed =
+                    RadiansPerSecond.of(
+                            0.1
+                                    + intakeArmFeedforward.calculate(
+                                            intakeArmAbsoluteEncoder.getPosition(), 0.1));
+            intakeArmSparkFlex.set(desiredSpeed.in(RotationsPerSecond));
+
         } else {
-            intakeArmClosedLoop.setSetpoint(0, ControlType.kVelocity);
+            // intakeArmClosedLoop.setSetpoint(0, ControlType.kVelocity);
+            AngularVelocity desiredSpeed =
+                    RadiansPerSecond.of(
+                            0
+                                    + intakeArmFeedforward.calculate(
+                                            intakeArmAbsoluteEncoder.getPosition(), 0));
+            intakeArmSparkFlex.set(desiredSpeed.in(RotationsPerSecond));
         }
     }
 
     @Override
     public void updateState(IntakeArmIOState state) {
-        // if (TUNABLE_KS.hasChanged()) {
-        // intakeArmClosedLoopConfig.feedForward.kS(TUNABLE_KS.get());
-        // System.out.println("allegedly changing ks to " + TUNABLE_KS.get());
-        // intakeArmSparkFlexConfig.apply(intakeArmClosedLoopConfig);
-        // intakeArmSparkFlex.configure(
-        // intakeArmSparkFlexConfig,
-        // ResetMode.kResetSafeParameters,
-        // PersistMode.kNoPersistParameters);
+        // if (TUNABLE_INTAKE_KCOS.hasChanged()) {
+        //     intakeArmClosedLoopConfig.feedForward.kCos(TUNABLE_INTAKE_KCOS.get());
+        //     System.out.println("allegedly changing ks to " + TUNABLE_INTAKE_KCOS.get());
+        //     intakeArmSparkFlexConfig.apply(intakeArmClosedLoopConfig);
+        //     intakeArmSparkFlex.configure(
+        //             intakeArmSparkFlexConfig,
+        //             ResetMode.kResetSafeParameters,
+        //             PersistMode.kPersistParameters);
         // }
 
         state.desiredAngleDegrees = desiredAngle.in(Degrees);
-        state.actualAngleDegrees = intakeArmAbsoluteEncoder.getPosition();
-        state.velocityDegreesPerSecond = intakeArmAbsoluteEncoder.getVelocity();
+        state.actualAngleDegrees = intakeArmAbsoluteEncoder.getPosition() * (180 / Math.PI);
+        state.velocityDegreesPerSecond = intakeArmAbsoluteEncoder.getVelocity() * (180 / Math.PI);
         state.appliedVoltage =
                 intakeArmSparkFlex.getBusVoltage() * intakeArmSparkFlex.getAppliedOutput();
         state.current = intakeArmSparkFlex.getOutputCurrent();
