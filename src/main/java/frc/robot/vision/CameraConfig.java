76968dc42a34e65a168828fec54526d0f7319c85
mhoughton26 <mhoughton26@hb.edu>
2026-03-10 19:58:39 -0400

    insert comp camera config, z could be wrong because not sure if it's to ground or bellypan

--- src/main/java/frc/robot/vision/CameraConfig.java
+++ src/main/java/frc/robot/vision/CameraConfig.java
@@ -71,6 +71,21 @@ public class CameraConfig {
                 SECOND_Y_ROBOT_TO_CAMERA_OFFSET = Inches.of(0);
                 SECOND_Z_ROBOT_TO_CAMERA_OFFSET = Inches.of(0);
                 break;
+            case COMP:
+                LIMELIGHT_NAME = "";
+                CAMERA_PITCH = Degrees.of(32.794);
+                CAMERA_YAW = Degrees.of(80.063);
+                X_ROBOT_TO_CAMERA_OFFSET = Inches.of(12.516);
+                Y_ROBOT_TO_CAMERA_OFFSET = Inches.of(-9.351);
+                Z_ROBOT_TO_CAMERA_OFFSET = Inches.of(17.903);
+                HAS_SECOND_CAMERA = false;
+                SECOND_LIMELIGHT_NAME = "not_a_limelight";
+                SECOND_CAMERA_PITCH = Degrees.of(0);
+                SECOND_CAMERA_YAW = Degrees.of(0);
+                SECOND_X_ROBOT_TO_CAMERA_OFFSET = Inches.of(0);
+                SECOND_Y_ROBOT_TO_CAMERA_OFFSET = Inches.of(0);
+                SECOND_Z_ROBOT_TO_CAMERA_OFFSET = Inches.of(0);
+                break;
             default:
                 LIMELIGHT_NAME = "";
                 CAMERA_PITCH = Degrees.of(0);
