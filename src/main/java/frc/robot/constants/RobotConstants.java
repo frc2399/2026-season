6104f1c0c88ca0c3ba05a5c19fad29e8ce829021
mhoughton26 <mhoughton26@hb.edu>
2026-03-26 19:28:43 -0400

    various ideas, maybe we negative a negative?

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -69,8 +69,8 @@ public final class RobotConstants {
     }
 
     public static class TransformConstants {
-        public static final Distance ROBOT_TO_SHOOTER_X = Inches.of(-10.425726);
-        public static final Distance ROBOT_TO_SHOOTER_Y = Inches.of(-8.5);
+        public static final Distance ROBOT_TO_SHOOTER_X = Inches.of(10.425726);
+        public static final Distance ROBOT_TO_SHOOTER_Y = Inches.of(8.5);
         // these values should be edited once we have our robot to shooter transform
         // DO NOT LET THESE CHANGES GO TO MAIN THEY ARE FOR TESTING
         public static final Transform2d ROBOT_TO_SHOOTER_TRANSFORM =
