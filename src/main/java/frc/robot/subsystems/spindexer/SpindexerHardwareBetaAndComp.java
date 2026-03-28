b53792c9288630ada1d614189f084d3d3319c170
mhoughton26 <mhoughton26@hb.edu>
2026-03-28 13:26:06 -0400

    noodling

--- src/main/java/frc/robot/subsystems/spindexer/SpindexerHardwareBetaAndComp.java
+++ src/main/java/frc/robot/subsystems/spindexer/SpindexerHardwareBetaAndComp.java
@@ -95,10 +95,7 @@ public class SpindexerHardwareBetaAndComp implements SpindexerIO {
 
     public void runSpindexer() {
         desiredVelocity =
-                RadiansPerSecond.of(
-                        0.085
-                                * SPINDEXER_GEAR_RATIO
-                                * MotorConstants.VORTEX_FREE_SPEED.in(RadiansPerSecond));
+                RadiansPerSecond.of(0.85 * MotorConstants.VORTEX_FREE_SPEED.in(RadiansPerSecond));
         spindexerPidController.setSetpoint(
                 desiredVelocity.in(RadiansPerSecond), ControlType.kVelocity);
     }
