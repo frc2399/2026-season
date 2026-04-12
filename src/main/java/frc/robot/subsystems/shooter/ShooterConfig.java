9a0cc73ec7366771d85329c903db07cfde49da1d
mhoughton26 <mhoughton26@hb.edu>
2026-04-12 13:48:07 -0400

    happy rollers

--- src/main/java/frc/robot/subsystems/shooter/ShooterConfig.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConfig.java
@@ -28,10 +28,10 @@ public class ShooterConfig {
             case COMP:
                 BOTTOM_KS = .122;
                 BOTTOM_KV = 0.0171;
-                BOTTOM_P = 0.005;
+                BOTTOM_P = 0.003;
                 TOP_KS = .155;
                 TOP_KV = 0.017;
-                TOP_P = 0.00003;
+                TOP_P = 0.000005;
                 BOTTOM_ROLLER_INVERTED = true;
                 break;
             default:
