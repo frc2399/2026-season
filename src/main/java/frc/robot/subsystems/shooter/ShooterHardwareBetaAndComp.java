59f3cc9c397c6176516a3c858dc1ae12563f38b2
mhoughton26 <mhoughton26@hb.edu>
2026-04-25 13:03:26 -0400

    remove extraneous lines

--- src/main/java/frc/robot/subsystems/shooter/ShooterHardwareBetaAndComp.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterHardwareBetaAndComp.java
@@ -150,20 +150,18 @@ public class ShooterHardwareBetaAndComp implements ShooterIO {
 
     public void runShooterWithSpeeds(AngularVelocity topSpeed, AngularVelocity bottomSpeed) {
 
-        if (bottomSpeed.in(RPM) < RobotConstants.MotorConstants.NEO_FREE_SPEED.in(RPM)) {
+        if (bottomSpeed.in(RPM) < RobotConstants.MotorConstants.VORTEX_FREE_SPEED.in(RPM)) {
             desiredBottomVelocity = bottomSpeed;
         } else {
-            desiredBottomVelocity = RobotConstants.MotorConstants.NEO_FREE_SPEED;
+            desiredBottomVelocity = RobotConstants.MotorConstants.VORTEX_FREE_SPEED;
         }
 
-        if (topSpeed.in(RPM) < RobotConstants.MotorConstants.NEO_FREE_SPEED.in(RPM)) {
+        if (topSpeed.in(RPM) < RobotConstants.MotorConstants.VORTEX_FREE_SPEED.in(RPM)) {
             desiredTopVelocity = topSpeed;
         } else {
-            desiredTopVelocity = RobotConstants.MotorConstants.NEO_FREE_SPEED;
+            desiredTopVelocity = RobotConstants.MotorConstants.VORTEX_FREE_SPEED;
         }
-
-        desiredBottomVelocity = bottomSpeed;
-        desiredTopVelocity = topSpeed;
+        
         shooterBottomPIDController.setSetpoint(
                 desiredBottomVelocity.in(RadiansPerSecond), ControlType.kVelocity);
         shooterTopPIDController.setSetpoint(
